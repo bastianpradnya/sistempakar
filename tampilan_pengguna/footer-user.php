c70cd3625fac804471257f34dfa9fa70b980e8be
Agung Satria <satria.pw@gmail.com>
2018-06-08 15:50:13 +0700

    penambahai item pada footer
    menginputkan tahun copyright 2018

--- tampilan_pengguna/footer-user.php
+++ tampilan_pengguna/footer-user.php
@@ -1,5 +1,5 @@
 <!-- Footer -->
     <footer class="py-5 bg-black">
       <div class="container">
-        <p class="m-0 text-center text-white small">&copy; STMIK AKAKOM YOGYAKARTA - 135410106</p>
+        <p class="m-0 text-center text-white small">&copy;2018 STMIK AKAKOM YOGYAKARTA - 135410106</p>
       </div>
