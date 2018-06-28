db6ddcc3488a8fc416a7d33e25849e38b94f88e0
Agung Satria <satria.pw@gmail.com>
2018-06-28 14:43:13 +0700

    kaidah

--- tampilan_pakar/kaidah.php
+++ tampilan_pakar/kaidah.php
@@ -29,16 +29,14 @@
       </ol>
       <div class="row">
         <div class="col-12">
-          <h1>Kaidah Pengetahuan</h1>
-          <p>This is an example of a blank page that you can use as a starting point for creating new ones.</p>
+          <h1>Kaidah</h1>
         </div>
       </div>
-    </div>
 
       <!-- Example DataTables Card-->
       <?php
 	      include "koneksi.php";
-	      $sql = "select * from basis_pengetahuan order by kode_penyakit";
+	      $sql = "select * from basis_pengetahuan group by kode_penyakit order by kode_penyakit";
 	      $hasil = mysqli_query($konek, $sql);
 	      if(!$hasil){
 		      die ("Gagal Query..".mysqli_error($konek));
@@ -46,12 +44,16 @@
       ?>
       <div class="card mb-3">
         <div class="card-header">
-          <i class="fa fa-table"></i> Tabel Solusi Penyakit</div>
+          <i class="fa fa-table"></i> Tabel Kaidah Sistem Pakar</div>
+          <div class="" align="right" style="padding:15px;">
+            <button class="btn btn-primary" data-toggle="modal" data-target="#tambah">Tambah Data Kaidah</button>
+          </div>
         <div class="card-body">
           <div class="table-responsive">
             <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
               <thead>
                 <tr>
+                  <th>ID</th>
                   <th>Kode Penyakit</th>
                   <th>Nama Penyakit</th>
                   <th>Gejala</th>
@@ -60,19 +62,21 @@
               <tbody>
               <?php while ($row = mysqli_fetch_assoc($hasil)){ ?>
 			                 <tr>
-                        <td><?php echo $row['kode_penyakit'] ?></td>
+                        <td><?php echo $row['id_pengetahuan'] ?></td>
+  			                 <td><?php echo $row['kode_penyakit'] ?></td>
                          <td>
-                          <?php
-                            $kodeP = $row['kode_penyakit'];
-                            $sql2 = "select nama_penyakit from penyakit where kode_penyakit='".$kodeP."'";
-                            $tampil = mysqli_query($konek, $sql2);
-                            $namaP = mysqli_fetch_assoc($tampil);
-                          ?>
                           <?php 
-                            echo $namaP['nama_penyakit'];
+                            include "koneksi.php";
+                            $sql2 = "select nama_penyakit from penyakit where kode_penyakit='".$row['kode_penyakit']."'";
+                            $nama = mysqli_query($konek, $sql2);
+                            $tampil = mysqli_fetch_assoc($nama);
+                            echo $tampil['nama_penyakit'];
                           ?>
                          </td>
-                         <td><button class="btn btn-info btn-sm" data-toggle="modal" data-target="#kaidah">Detail</button></td>
+                         <td>
+                          <button class="btn btn-info btn-sm" data-toggle="modal" data-target="#kaidah<?php echo $row['kode_penyakit'] ?>">Lihat Gejala</button>&nbsp;&nbsp;
+                          <button class="btn btn-danger btn-sm" data-toggle="modal" data-target="#hapus<?php echo $row['kode_penyakit'] ?>">Hapus</button>
+                         </td>
                        </tr>
                 <?php } ?>
               </tbody>
@@ -83,37 +87,152 @@
     </div>
   </div>
    <!-- /.container-fluid-->
- 
-    <!-- Kaidah Penyakit Modal-->
-    <div class="modal" id="kaidah" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+
+    <!-- Tambah Modal-->
+    <div class="modal" id="tambah" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
           <div class="modal-dialog" role="document">
             <div class="modal-content">
               <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">Kaidah Penyakit</h5>
+                <h5 class="modal-title" id="exampleModalLabel">Tambah Informasi Penyakit</h5>
                 <button class="close" type="button" data-dismiss="modal" aria-label="Close">
                   <span aria-hidden="true">×</span>
                 </button>
               </div>
               <div class="modal-body">     
-                <form name="update" method="post" action="simpan_gejala.php">
+              <form action="tambah_solusi.php" method="post">
                 <div class="form-group">
-                  <label for="comment">Informasi Penyakit <?php echo $row['nama_penyakit'] ?> </label>
-                  <textarea class="form-control" rows="5" id="comment"></textarea>
-                </div>
-                <div class="form-group">
-                  <label for="comment">Solusi Penanganan <?php echo $row['nama_penyakit'] ?> </label>
-                  <textarea class="form-control" rows="1 5" id="comment"></textarea>
-                </div>
-                  <button class="btn btn-info" type="submit">Tambah</button>
-                  <button class="btn btn-info" type="button">Ubah</button>
-                  <button class="btn btn-info" type="button">Hapus</button>
+                <?php
+                  include "koneksi.php";
+                  $sql3 = "select * from penyakit order by kode_penyakit";
+                  $nama2 = mysqli_query($konek, $sql3);
+                ?>
+                  <label for="sel1">Pilih Penyakit:</label>
+                    <select class="form-control" name="sel1">
+                    <?php while ($row3 = mysqli_fetch_assoc($nama2)){ ?>
+                      <option><?php echo $row3['nama_penyakit']?></option>
+                      <?php } ?>
+                    </select>
+                  </div>
+                  <button class="btn btn-info" type="submit">Lanjutkan</button>
                   <button class="btn btn-danger" type="button" data-dismiss="modal" aria-label="Close">Batal</button>
                 </form>
+              </div>
+          </div>
+          </div>
+          </div>
+    </div>
+
+      <?php
+        include "koneksi.php";
+        $sql = "select * from basis_pengetahuan group by kode_penyakit order by kode_penyakit";
+        $hasil = mysqli_query($konek, $sql);
+        if(!$hasil){
+          die ("Gagal Query..".mysqli_error($konek));
+        }
+      ?>
+
+       <?php while ($row = mysqli_fetch_assoc($hasil)) { ?>
+        <!-- Kaidah Modal-->
+        <div class="modal" id="kaidah<?php echo $row['kode_penyakit'] ?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+          <div class="modal-dialog" role="document">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Gejala Penyakit  
+                  <?php 
+                      include "koneksi.php";
+                      $sql2 = "select nama_penyakit from penyakit where kode_penyakit='".$row['kode_penyakit']."'";
+                      $nama = mysqli_query($konek, $sql2);
+                      $tampil = mysqli_fetch_assoc($nama);
+                      echo $tampil['nama_penyakit'];
+                  ?>
+                </h5>
+                <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">×</span>
+                </button>
+              </div>
+              <?php
+                include "koneksi.php";
+                $sql = "select * from basis_pengetahuan";
+                $hasil = mysqli_query($konek, $sql);
+                if(!$hasil){
+                  die ("Gagal Query..".mysqli_error($konek));
+                }
+              ?>
+              <div class="modal-body">     
+                <form action="edit_kaidah.php" method="post">
+                  <div class="table-responsive">
+                    <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                      <thead>
+                        <tr>
+                        <th>ID</th>
+                        <th>Nama Gejala</th>
+                        <th>Nilai</th>
+                        <th>Operasi</th>
+                      </tr>
+                      </thead>
+                      <tbody>
+                        <?php while ($row = mysqli_fetch_assoc($hasil)){ ?>
+                          <tr>
+                            <td><?php echo $row['id_pengetahuan'] ?></td>
+                            <td>
+                              <?php 
+                                include "koneksi.php";
+                                $sql2 = "select nama_gejala from gejala where kode_gejala='".$row['kode_gejala']."'";
+                                $nama = mysqli_query($konek, $sql2);
+                                $tampil = mysqli_fetch_assoc($nama);
+                                echo $tampil['nama_gejala'];
+                              ?>
+                            </td>
+                            <td><?php echo $row['nilai_belief'] ?></td>
+                            <td>
+                              <button class="btn btn-info btn-sm" data-toggle="modal" data-target="#<?php echo $row['kode_penyakit'] ?>">Ubah</button>&nbsp;&nbsp;
+                              <button class="btn btn-danger btn-sm" data-toggle="modal" data-target="#<?php echo $row['kode_penyakit'] ?>">Hapus</button>
+                            </td>
+                          </tr>
+                        <?php } ?>
+                    </tbody>
+                  </table>
+                </div>
+              </form>
             </div>
           </div>
           </div>
+        </div>
+      </div>
+      <?php } ?>
+
+     <?php
+        include "koneksi.php";
+        $sql = "select * from penyakit order by kode_penyakit";
+        $hasil = mysqli_query($konek, $sql);
+        if(!$hasil){
+          die ("Gagal Query..".mysqli_error($konek));
+        }
+      ?>
+
+    <?php while ($row = mysqli_fetch_assoc($hasil)) { ?>
+      <!-- Hapus Modal-->
+        <div class="modal" id="hapus<?php echo $row['kode_penyakit'] ?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+          <div class="modal-dialog" role="document">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Peringatan</h5>
+                <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">×</span>
+                </button>
+              </div>
+              <div class="modal-body">
+              Yakin ingin menghapus data <?php echo $row['nama_penyakit'] ?> ?
+            </div>
+            <div class="modal-footer">
+              <a href=hapus_solusi.php?kode=<?php echo $row['kode_penyakit'] ?> class="btn btn-primary">Ya</a>
+              <button class="btn btn-danger" type="button" data-dismiss="modal" aria-label="Close">Batal</button>
+            </div>
           </div>
           </div>
+        </div>
+    <?php } ?>
+
 
    <?php 
       include "footer.php";
