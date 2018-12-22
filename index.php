f9484d7cfeef457f84642a58bede8c9a8acf8355
Satria Pradnya <satria.pw@gmail.com>
2018-12-22 22:28:13 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Index</title>
+</head>
+<body>
+    <?php
+      echo '<META HTTP-EQUIV="Refresh" Content="0; URL=tampilan-pengguna/index.php">';
+    ?>
+</body>
+</html>
